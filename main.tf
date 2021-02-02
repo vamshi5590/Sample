4720293624b92f8133beae608595dc919f48de04
vamshi5590 <53261277+vamshi5590@users.noreply.github.com>
2021-02-02 17:26:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "var.access_key"
-  secret_key = "var.secret_key"
-  region = "var.region"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
 }
 
 #Virtual Private Cloud
@@ -41,30 +41,32 @@ resource "aws_route_table" "ngw" {
 resource "aws_route" "ngw" {
   route_table_id = aws_route_table.ngw.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.main1.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
+
 #EIP Assign for NAT Gateway
 resource "aws_eip" "nat" {
   vpc = true
 }
-resource "aws_nat_gateway" "main1" {
+
+resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.private-us-east-2a.id
+  subnet_id = aws_subnet.public-us-east-2a.id
   tags = {
     Name = "${var.project_name}${var.env}"
       }
 }
-resource "aws_nat_gateway" "main2" {
-  allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.private-us-east-2b.id
-  tags = {
-    Name = "${var.project_name}${var.env}"
-      }
-}
+#resource "aws_nat_gateway" "main2" {
+#  allocation_id = aws_eip.nat.id
+#  subnet_id = aws_subnet.private-us-east-2b.id
+#  tags = {
+#    Name = "${var.project_name}${var.env}"
+#      }
+#}
 #### public- Subnet 2a
 resource "aws_subnet" "public-us-east-2a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "var.public_subnet_2a"
+  cidr_block = "${var.public_subnet_2a}"
   availability_zone_id = "use2-az1"
   tags = {
     Name = "public-us-east-2a-${var.project_name}${var.env}"
@@ -77,7 +79,7 @@ resource "aws_route_table_association" "public-us-east-2a" {
 #### public Subnet- 2b
 resource "aws_subnet" "public-us-east-2b" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "var.public_subnet_2b"
+  cidr_block = "${var.public_subnet_2b}"
   availability_zone_id = "use2-az2"
   tags = {
    Name = "public-us-east-2b-${var.project_name}${var.env}"
@@ -90,7 +92,7 @@ resource "aws_route_table_association" "public-us-east-2b" {
 #### private Subnet - 2a
 resource "aws_subnet" "private-us-east-2a" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "var.private_subnet_2a"
+  cidr_block = "${var.private_subnet_2a}"
   availability_zone_id = "use2-az1"
   tags = {
     Name = "private-us-east-2a-${var.project_name}${var.env}"
@@ -103,7 +105,7 @@ resource "aws_route_table_association" "private-us-east-2a" {
 #### private Subnet -2b
 resource "aws_subnet" "private-us-east-2b" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "private_subnet_2b"
+  cidr_block = "${var.private_subnet_2b}"
   availability_zone_id = "use2-az2"
   tags = {
     Name = "private-us-east-2b-${var.project_name}${var.env}"
